866d2ce7a0479856336a5a31b87dd12d07176249
Yurashk <yurahi@ukr.net>
2026-09-17 18:07:05 +0200

    Level 3 rare drop: 5% chance

--- src/main.js
+++ src/main.js
@@ -364,7 +364,7 @@ function updatePreview() {
 }
 
 function getRandomLowLevelSlime() {
-  if (Math.random() < 0.15) return SLIME_CONFIGS[2];
+  if (Math.random() < 0.05) return SLIME_CONFIGS[2];
   return SLIME_CONFIGS[Math.floor(Math.random() * 2)];
 }
 
